66f1a139187fd0f7b229dde2a10cf5848ac6c6da
Bridget McCavanagh <bridget@Daves-MacBook-Pro.local>
2022-03-03 11:51:49 +0800

    Authentication completed.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,52 @@
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
 const resolvers = {
     Query: {
-        books
+        me: async (parent, args, context) => {
+            // context.user
+            return context.user;
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            console.log(token);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email: body.email });
+            if (!user) {
+                throw new Error("Can't find this user");
+            }
+
+            const correctPw = await user.isCorrectPassword(body.password);
+
+            if (!correctPw) {
+                throw new Error("Wrong password!");
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { authors, description, title, bookId, image, link }) => {
+            // return User
+        },
+        removeBook: async (parent, { bookId }) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { new: true }
+            );
+            if (!updatedUser) {
+                return res.status(404).json({ message: "Couldn't find user with this id!" });
+            }
+            return updatedUser;
+            // return User
+        }
     }
 }
+
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
+require ('dotenv').config();
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+
+const typeDefs = require('./schemas/typeDefs');
+const resolvers = require('./schemas/resolvers');
+
+// Import `authMiddleware()` function to be configured with the Apollo Server
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // Add context to our server so data from the `authMiddleware()` function can pass data to our resolver functions
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
