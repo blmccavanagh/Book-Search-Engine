61897ce8f9e5778a3d8b1f91fb3fb764b5b40f26
Bridget McCavanagh <bridget@Daves-MacBook-Pro.local>
2022-05-01 12:39:18 +0800

    Login form updated for use with apollo graph ql.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,15 +2,17 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
-  
+// Add mutation
+  const [loginUser, { error, data }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -27,15 +29,35 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
-    try {
-      const response = await loginUser(userFormData);
+    // restful API
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+    // try {
+    //   const response = await loginUser(userFormData);
+
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+
+    //   const { token, user } = await response.json();
+    //   console.log(user);
+    //   Auth.login(token);
+    // } catch (err) {
+    //   console.error(err);
+    //   setShowAlert(true);
+    // }
+
+    // update to use apollo graph ql
+
+    try {
+      const { data } = await loginUser({
+        variables: { ...userFormData }
+      })
+
+      if (!data) {
+        throw new Error('login failed!');
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
+      const { token, user } = data.login;
       Auth.login(token);
     } catch (err) {
       console.error(err);
