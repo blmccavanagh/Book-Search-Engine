611aee02acda2e7516bb9e1e248fc78466de629c
Bridget McCavanagh <bridget@Daves-MacBook-Pro.local>
2022-05-01 21:45:09 +0800

    typeDefs updated to match with mutations.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,17 +5,18 @@ const typeDefs = gql`
     _id: ID!
     username: String!
     email: String!
+    password: String!
     bookCount: Int!
     savedBooks: [Book]
   }
 
   type Book {
-    bookId: String!
     authors: [String]
     description: String!
-    title: String!
+    bookId: String!
     image: String!
     link: String!
+    title: String!
   }
 
   type Auth {
@@ -24,14 +25,16 @@ const typeDefs = gql`
   }
 
   type Query {
+    getSingleUser(id: ID!): User
     me: User
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(authors: [String!], description: String!, title: String!, bookId: String!, image: String!, link: String!): User
+    saveBook(authors: [String], description: String!, bookId: String!, image: String, link: String, title: String!): User
     removeBook(bookId: String!): User
+  }
   }
 `;
 
